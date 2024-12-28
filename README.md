12cd1081dabcf4eef7b50a94b3e3c59718e90d17
Kevin <370865552@qq.com>
2024-12-28 11:00:55 +0800

    First

---
+++ README.md
@@ -0,0 +1,2 @@
+# LuoguDesktop
+ LuoguDesktop
---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, BrowserView, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const app = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  app.loadURL("https://www.luogu.com.cn")
+  app.menu
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
